f945eff530cefde69f4eff4b8ccea04ad61df43c
Igor Tessaro <igorstessaro57@gmail.com>
2018-10-08 10:22:35 -0300

    Ajustado agendador de tarefas.

--- src/main/java/br/com/agibank/appanalisedados/AppAnaliseDadosApplication.java
+++ src/main/java/br/com/agibank/appanalisedados/AppAnaliseDadosApplication.java
@@ -2,8 +2,12 @@ package br.com.agibank.appanalisedados;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ComponentScan({"br.com.agibank.appanalisedados"})
+@EnableScheduling
 public class AppAnaliseDadosApplication {
 
 	public static void main(String[] args) {
